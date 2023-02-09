ca27e892e222d405fcd7f776ccb185cd9eb0583f
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-02-09 17:14:47 +0000

    finished state

--- js/ main.js
+++ js/ main.js
@@ -1,6 +1,8 @@
 let headingText = document.getElementById('headingText');
 let nextButton = document.getElementById('nextButton');
+let revealButton = document.getElementById('revealButton');
 let subText = document.getElementById('subText');
+let proceed = document.getElementById('proceed');
 let goButton = document.getElementById('goButton');
 let returnButton = document.getElementById('returnButton');
 
@@ -21,12 +23,12 @@ returnButton.addEventListener("click", myFunction);
 // need a function when to activate what button
 
 function show(object) {
-object.style.visibility = 'visible';
+    object.style.visibility = 'visible';
 }
 
 function hide(object) {
     object.style.visibility = 'hidden';
-    }
+}
 
 // create array with symbols and when to display what symbol based on number
 
@@ -38,36 +40,62 @@ function hide(object) {
 function state() {
 
     switch (page) {
-    case 0:
-    show(goButton)
-    headingText.innerHTML = "I can read your mind"
-    break;
+        case 0:
+            headingText.innerHTML = "I can read your mind!";
+            hide(nextButton);
+            hide(revealButton);
+            show(goButton);
+            hide(returnButton);
+            break;
 
-    case 1:
-    // atoms and molecules
+        case 1:
+            headingText.innerHTML = "Pick a number from 01 - 99";
+            show(nextButton);
+            hide(revealButton);
+            subText.innerHTML = "when you have your number click next";
+            hide(goButton);
+            show(returnButton);
+            break;
 
+        case 2:
+            headingText.innerHTML = "Add both digits together to get a new number";
+            show(nextButton);
+            hide(revealButton);
+            subText.innerHTML = "Ex. 14 is 1 + 4 = 5";
+            proceed.innerHTML = "click next to proceed";
+            hide(goButton);
+            show(returnButton);
+            break;
 
-    break;
-    case 2:
-    // atoms and molecules
+        case 3:
+            headingText.innerHTML = "Subtract your new number from the original number";
+            show(nextButton);
+            hide(revealButton);
+            subText.innerHTML = "Ex. 14 - 5 = 9";
+            proceed.innerHTML = "click next to proceed";
+            hide(goButton);
+            show(returnButton);
+            break;
 
+        case 4:
+            headingText.innerHTML = "0 - & <br> 1 - @ <br> 2 - $ <br> 3 - B <br> ...";
+            show(revealButton);
+            hide(nextButton);
+            subText.innerHTML = "Find your new number";
+            proceed.innerHTML = "Note the symbol beside the number";
+            hide(goButton);
+            show(returnButton);
+            break;
 
-    break;
-    case 3:
-    // atoms and molecules
-
-
-    break;
-    case 4:
-    // atoms and molecules
-
-
-    break;
-    case 5:
-    // atoms and molecules
-
-
-    break;
-} 
+        case 5:
+            headingText.innerHTML = "&";
+            hide(revealButton);
+            hide(nextButton);
+            subText.innerHTML = "Your symbol is:";
+            proceed.innerHTML = "&";
+            hide(goButton);
+            show(returnButton);
+            break;
+    }
 }
 
