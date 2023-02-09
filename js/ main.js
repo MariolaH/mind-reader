0fb47cd3549998c9d635066138b5c7488d3e6071
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-02-09 01:55:51 +0000

    added JS

---
+++ js/ main.js
@@ -0,0 +1,16 @@
+let headingText = document.getElementById(headingText);
+let nextButton = document.getElementById(nextButton);
+let subTextt = document.getElementById(subText);
+let goButtont = document.getElementById(goButtont);
+
+
+
+
+// need to create event listners for NextButton to go to next page
+
+// need to create event listners for goButton to go to next page.... but return to page 1 on page 2,3,4,5,6,
+
+
+// create state with objects with their properties and values
+
+// create cards with objects and when to call the values 
